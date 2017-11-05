34b843d3e1b540fa0b0e7847083c4f132747ef71
firsou <guschefi@student.gu.se>
2017-11-05 20:58:09 +0100

    Linked Pressure Plate to Doors and Default Pawn

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -19,11 +19,14 @@ UOpenDoor::UOpenDoor()
 void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
+	// ...
+	
+}
+
+void UOpenDoor::OpenDoor() {
 	AActor* Owner = GetOwner();
 	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
 	Owner->SetActorRelativeRotation(NewRotation);
-	// ...
-	
 }
 
 
@@ -32,6 +35,8 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+		OpenDoor();
+	}
 }
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -21,6 +21,8 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void OpenDoor();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -28,7 +30,10 @@ public:
 private:
 	UPROPERTY(VisibleAnywhere)
 	float OpenAngle = 90.0f;
+
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate;
-	
+
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens; //Remember Pawn inherits from Actor
 };
